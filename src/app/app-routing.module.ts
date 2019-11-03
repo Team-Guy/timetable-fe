c48b4475f059fb1c8c8e3442aea995ac9d26fd67
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-11-03 09:22:13 +0200

    almost done, todo: fix select div placement

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
+import { SelectGroupPageComponent } from './select-group-page/select-group-page.component';
+
+const appRoutes: Routes = [
+  {path: 'selectgroup', component: SelectGroupPageComponent},
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+  constructor(private router: Router){}
+ }
+export const routingComponents = [SelectGroupPageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule,MatInputModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
